5da9e13b3ddd3ff267217d47603240cb9c4ff948
hhey-jay <byeol4365@naver.com>
2024-05-26 00:00:47 +0900

    not found and detail

---
+++ Desktop/Week4/MovieSite/src/Card/Card.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './Card.css'
+//const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const  IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
+
+function Card({title, poster_path, vote_average, overview}) {
+
+    return(
+        <div className='movie-container'>
+            <div className='movie-box'>
+                <img src={IMG_BASE_URL + poster_path} alt='영화포스터'></img>
+                <div className='movie-overview'>
+                    <p>{title}</p>
+                    <p>{overview}</p>
+                </div>
+                <div className='movie-info'>
+                    <h4>{title}</h4>
+                    <span>{vote_average}</span>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Card;
