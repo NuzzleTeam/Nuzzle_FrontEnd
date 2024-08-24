5aba97b399c55045fdc51dbf6134eb6691e54c92
MONG-SIL <abok2836@naver.com>
2024-08-24 14:14:23 +0900

    asdf

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
   server: {
     port: 3000
+  },
+  base: '/',
+  build: {
+    chunkSizeWarningLimit: 1100, // 경고 기준을 1000KB로 조정
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux'], // 주요 라이브러리들을 별도의 청크로 분리
+        }
+      }
+    }
   },
 });
