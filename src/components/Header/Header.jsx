fca8a6f6c882b0c5b323c72311ab8c978951b785
gyuwonKim <gimgyuwon2@gmail.com>
2024-09-12 07:40:46 -0700

    refactor: Update useless code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,39 @@
+import React from "react";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Header.css";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+  height: 6vh;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fcfdf5;
+  color: #fcfdf5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1px 20px;
+  z-index: 100;
+
+  &.custom-header-bg {
+    background-color: #f3f3f3;
+  }
+`;
+
+const BackIcon = styled(FontAwesomeIcon)`
+  font-size: 20px;
+  margin-right: 5px;
+  color: rgba(0, 0, 0, 1);
+
+  &:active {
+    color: black;
+    transform: scale(1.1);
+  }
+`;
 
 function Header({ extraClass }) {
   let navigate = useNavigate();
@@ -12,15 +43,11 @@ function Header({ extraClass }) {
   };
 
   return (
-    <header className={`header ${extraClass}`}>
+    <HeaderWrapper className={extraClass}>
       <div className="back">
-        <FontAwesomeIcon
-          icon={faAngleLeft}
-          className="back-icon"
-          onClick={goBack}
-        />
+        <BackIcon icon={faAngleLeft} onClick={goBack} />
       </div>
-    </header>
+    </HeaderWrapper>
   );
 }
 
